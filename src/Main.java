3bee8bb2d840b9974552cc8aaad3f3a27c2074d7
Verkin78 <otrajenie.jc@gmail.com>
2022-12-16 20:46:50 +0300

    Chess

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        public static void example() {
+            List<List<String>> biDemArrList = new ArrayList<>();
+            for (int i = 0; i < 8; i++) {
+                biDemArrList.add(i, new ArrayList<>());
+                for (int j = 0; j < 8; j++) {
+                    biDemArrList.get(i).add(j, (i + j) % 2 == 1 ? "●" : "◯");
+                }
+            }
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    System.out.print(biDemArrList.get(i).get(j) + " ");
+
+                }
+                System.out.println();
+            }
+        }
     }
 }
